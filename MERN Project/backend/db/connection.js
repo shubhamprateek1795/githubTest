4071e7a28f0e3ab876e6c113e0003c200380edbd
shubhamprateek1795 <117595030+shubhamprateek1795@users.noreply.github.com>
2023-05-03 13:48:02 +0530

    quick update

--- MERN Project/backend/db/connection.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/mern_project')
-.then(()=>{
-    console.log("connected to Mongodb server")
-})
-.catch(()=>{
-    console.log("connection error in Mongodb server")
-})
