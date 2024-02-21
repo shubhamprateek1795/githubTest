9dfce663039cbc2aa9ac681264734887c0a56159
shubhamprateek1795 <shubhamprateek28@gmail.com>
2024-02-21 13:55:48 +0530

    react static app

--- mern project/backend/db/connection.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/mern_app')
-.then(()=>{
-  console.log("successfully connected to mongodb server")
-})
-.catch(()=>{
-    console.log("connection error in mongodb server")
-})
