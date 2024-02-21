d53a9e2556c75cc88269370655f5baf705199ae5
shubhamprateek1795 <117595030+shubhamprateek1795@users.noreply.github.com>
2024-02-21 10:57:03 +0530

    Delete mern project directory

--- mern project/backend/db/conn.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/mern_application')
-.then(()=>{
-    console.log("succesfully connected to mongoDb")
-})
-.catch(()=>{
-    console.log("connection error")
-})
--- mern project/frontend/src/components/PrivateComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-function privateComponent() {
-    const authToken=sessionStorage.getItem("token")
-  return authToken?<Outlet/>:<Navigate to="/login"/>
-}
-
-export default privateComponent
