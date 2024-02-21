b7e7df4aac302d9e75a549c907b53a4b03a81911
shubhamprateek1795 <117595030+shubhamprateek1795@users.noreply.github.com>
2024-02-21 10:55:26 +0530

    Delete express registration page using mongodb directory

--- express registration page using mongodb/db/conn.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/guardians_of_the_galaxy')
-.then(()=>{
-    console.log("successfully connected to mongoDB Server")
-})
-.catch(()=>{
-    console.log("connection error in mongoDB")
-})
